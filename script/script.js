e1e8a70ca2840e9885f7930f402a2aa67c2bc585
Daryl Chetty <chetty.daryl@gmail.com>
2020-10-24 11:33:45 +0200

    finally got the header navbar working

--- script/script.js
+++ script/script.js
@@ -1,37 +1,3 @@
-/*const navSlide = () => 
-{
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-bar');
-  const navLinks = document.querySelectorAll('.nav-bar li');
-  
-  burger.addEventListener('click', () => 
-  {
-    // Toggle burger nav menu
-    nav.classList.toggle('nav-active');
-
-    // Animate the nav links
-    navLinks.forEach((link, index) => 
-    {
-      if(link.style.animation)
-      {
-        link.style.animation = ''
-      } 
-      else
-      {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-      }
-    });
-
-    //Burger animation
-    burger.classList.toggle('toggle');
-
-  });  
-
-}
-
-navSlide();*/
-
-
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
 
